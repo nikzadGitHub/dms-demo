e1985e7e5eb0cb2c1fec545d8cf3bd5a08e48edd
Syahrul Radzi <syahrulradzi92@gmail.com>
2021-08-27 14:12:11 +0800

    Added index page template

---
+++ src/app/quote/index-template/index-template.component.ts
@@ -0,0 +1,51 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+
+@Component({
+  selector: 'app-index-template',
+  templateUrl: './index-template.component.html',
+  styleUrls: ['./index-template.component.scss']
+})
+export class IndexTemplateComponent implements OnInit {
+
+  @Input() quotes: Quote[];
+  @Input() title: string;
+  @Input() table_header: string;
+  paginate: [];
+  pageItems: number = 10;
+  search_text: string = '';
+
+  constructor() { }
+  
+  ngOnInit(): void {
+    // this.quoteService.getAll(this.pageItems,this.search_text).subscribe((data)=>{
+    //   this.quotes = data['data']['data'] ?? data['data']['items'];
+    //   this.paginate = data['data']['links'];
+    //   console.log(data);
+    // })  
+  }
+  
+  // deleteQuote(id){
+  //   this.quoteService.delete(id).subscribe(res => {
+  //        this.quotes = this.quotes.filter(item => item.id !== id);
+  //        console.log('Quote deleted successfully!');
+  //   })
+  // }
+
+  // getAll(){
+  //   this.quoteService.getAll(this.pageItems,this.search_text).subscribe((data)=>{
+  //     this.quotes = data['data']['data'] ?? data['data']['items'];
+  //     this.paginate = data['data']['links'];
+  //     console.log(data);
+  //   })  
+  // }
+
+  // onClick(url){
+  //   this.quoteService.getPage(url,this.pageItems,this.search_text).subscribe((data)=>{
+  //     this.quotes = data['data']['data'] ?? data['data']['items'];
+  //     this.paginate = data['data']['links'];
+  //     console.log(data);
+  //   })  
+  // }
+
+}
