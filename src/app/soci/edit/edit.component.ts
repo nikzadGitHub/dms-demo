872f9a24ab119bf0c8ffb277ef281e1825327403
Syahrul Radzi <syahrulradzi92@gmail.com>
2021-09-18 13:07:09 +0800

    SOCI edit

--- src/app/soci/edit/edit.component.ts
+++ src/app/soci/edit/edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Term } from '../../quote/create/terms';
 import { BillingList } from '../../quote/edit/billing-list';
@@ -16,8 +16,9 @@ import { SociService } from '../soci.service';
 export class EditComponent implements OnInit {
 
   filteredProducts: Product[];
-  enableEdit: boolean = false;
-  enableIndex: number = null;
+  controller: any;
+  selectedProductAdvanced: Product[] = [];
+  enableEdit: boolean[] = [];
   termSelected: number;
   quotations: Quote[];
   terms: Term[];
@@ -62,6 +63,10 @@ export class EditComponent implements OnInit {
   }
 
   setInitialValue(){
+    this.enableEdit['billing'] = false
+    this.enableEdit['payment'] = false
+    this.enableEdit['product'] = false
+    this.enableEdit['addCost'] = false
     this.company_details['company_name'] = this.quotations[0].company;
     this.company_details['quote_id'] = this.quotations[0].quote_id;
     this.f.standard_payment_term.setValue(this.quotations[0].standard_payment_term);
@@ -87,6 +92,7 @@ export class EditComponent implements OnInit {
       });
       element.products.forEach(product => {
         this.products().push(this.existingProducts(product));
+        this.selectedProductAdvanced.push(product)
       });
       this.subTotal(this.addCosts().controls);
     });
@@ -111,24 +117,24 @@ export class EditComponent implements OnInit {
 
     return this.formBuilder.group({
       'id': billing.id,
-      'billing_id': billing.billing_id,
-      'stage': billing.stage,
-      'percentage': billing.percentage,
-      'amount': billing.amount,
-      'status': billing.status,
-      'remarks': billing.remarks,
+      'billing_id': [{value:billing.billing_id,disabled:true}],
+      'stage': [{value:billing.stage,disabled:true}],
+      'percentage': [{value:billing.percentage,disabled:true}],
+      'amount': [{value:billing.amount,disabled:true}],
+      'status': [{value:billing.status,disabled:true}],
+      'remarks': [{value:billing.remarks,disabled:true}],
       'quote_id': billing.quote_id
     })
   }
 
   newBillings(): FormGroup {
     return this.formBuilder.group({
-      'billing_id': '',
-      'stage': '',
-      'percentage': '',
-      'amount': '',
-      'status': '',
-      'remarks': '',
+      'billing_id': [{value:'',disabled:true}],
+      'stage': [{value:'',disabled:true}],
+      'percentage': [{value:'',disabled:true}],
+      'amount': [{value:'',disabled:true}],
+      'status': [{value:'',disabled:true}],
+      'remarks': [{value:'',disabled:true}],
     })
   }
 
@@ -138,6 +144,21 @@ export class EditComponent implements OnInit {
    
   removeBillings(i:number) {
     this.billings().removeAt(i);
+  }
+
+  addBillingMilestone(){
+    var billing_id = this.billings().controls;
+    this.billingList = [];
+    billing_id.forEach(test=>{
+      // var data = test['controls']['billing_id'].value;
+      let bill = new BillingList;
+      bill['billing_id'] = test['controls']['billing_id'].value;
+      bill['amount'] = test['controls']['amount'].value;
+      this.billingList.push(bill);
+    });
+
+    this.billingList = this.billingList.map(item => item)
+    .filter((item, index, self) => self.indexOf(item) === index);
   }
 
   //---------------- End of Billings Milestone -------------------
@@ -151,25 +172,25 @@ export class EditComponent implements OnInit {
   existingPayments(payment): FormGroup {
     return this.formBuilder.group({
       'id': payment.id,
-      'billing_id': payment.billing_id,
-      'percentage': payment.percentage,
-      'schedule': payment.schedule,
-      'soc_payment_term': payment.soc_payment_term,
-      'amount': payment.amount,
-      'status': payment.status,
-      'remarks': payment.remarks,
+      'billing_id': [{value:payment.billing_id,disabled:true}],
+      'percentage': [{value:payment.percentage,disabled:true}],
+      'schedule': [{value:payment.schedule,disabled:true}],
+      'soc_payment_term': [{value:payment.soc_payment_term,disabled:true}],
+      'amount': [{value:payment.amount,disabled:true}],
+      'status': [{value:payment.status,disabled:true}],
+      'remarks': [{value:payment.remarks,disabled:true}],
     })
   }
 
   newPayments(): FormGroup {
     return this.formBuilder.group({
-      'billing_id': '',
-      'percentage': '',
-      'schedule': '',
-      'soc_payment_term': '',
-      'amount': '',
-      'status': '',
-      'remarks': '',
+      'billing_id': [{value:'',disabled:true}],
+      'percentage': [{value:'',disabled:true}],
+      'schedule': [{value:'',disabled:true}],
+      'soc_payment_term': [{value:'',disabled:true}],
+      'amount': [{value:'',disabled:true}],
+      'status': [{value:'',disabled:true}],
+      'remarks': [{value:'',disabled:true}],
     })
   }
 
@@ -192,22 +213,22 @@ export class EditComponent implements OnInit {
   existingCosts(addCosts): FormGroup {
     
     return this.formBuilder.group({
-      'id': addCosts.id,
-      'description': addCosts.description,
-      'quantity': addCosts.quantity,
-      'unit_price': addCosts.unit_price,
-      'total_price': addCosts.total_price,
-      'remarks': addCosts.remarks,
+      'id': new FormControl({value:addCosts.id,disabled:true},Validators.required),
+      'description': new FormControl({value:addCosts.description,disabled:true},Validators.required),
+      'quantity': new FormControl({value:addCosts.quantity,disabled:true},Validators.required),
+      'unit_price': new FormControl({value:addCosts.unit_price,disabled:true},Validators.required),
+      'total_price': new FormControl({value:addCosts.total_price,disabled:true},Validators.required),
+      'remarks': new FormControl({value:addCosts.remarks,disabled:true},Validators.required),
     })
   }
 
   newAddCosts(): FormGroup {
     return this.formBuilder.group({
-      'description': '',
-      'quantity': 0,
-      'unit_price': 0.00,
-      'total_price': 0.00,
-      'remarks': '',
+      'description': new FormControl({value:'',disabled:true},Validators.required),
+      'quantity': new FormControl({value:0,disabled:true},Validators.required),
+      'unit_price': new FormControl({value:0.00,disabled:true},Validators.required),
+      'total_price': new FormControl({value:0.00,disabled:true},Validators.required),
+      'remarks': new FormControl({value:'',disabled:true},Validators.required),
     })
   }
 
@@ -244,20 +265,40 @@ export class EditComponent implements OnInit {
     return this.form.get('products') as FormArray
   }
 
+  newProduct(){
+    return this.formBuilder.group({
+      'name': new FormControl({value:'',disabled:true},Validators.required),
+      'sku': new FormControl({value:'',disabled:true},Validators.required),
+      'quantity': new FormControl({value:'',disabled:true},Validators.required),
+      'unit_price': new FormControl({value:0.00,disabled:true},Validators.required),
+      'total_price': new FormControl({value:0.00,disabled:true},Validators.required),
+      'discount': new FormControl({value:0.00,disabled:true},Validators.required),
+      'amount': new FormControl({value:0.00,disabled:true},Validators.required)
+    })
+  }
+
   existingProducts(product){
     return this.formBuilder.group({
       'id': product.id,
       'external_product_number': product.external_product_number,
       'data_area_id': product.data_area_id,
       'quote_id': product.quote_id,
-      'name': [{value:product.name,disabled:true}],
-      'sku': [{value:product.sku,disabled:true}],
-      'quantity': [{value:product.quantity,disabled:true}],
-      'unit_price': [{value:product.unit_price,disabled:true}],
-      'total_price': [{value:(product.quantity * product.unit_price),disabled:true}],
-      'discount': [{value:product.discount,disabled:true}],
-      'amount': [{value:product.amount,disabled:true}],
+      'name': new FormControl({value:product.name,disabled:true},Validators.required),
+      'sku': new FormControl({value:product.sku,disabled:true},Validators.required),
+      'quantity': new FormControl({value:product.quantity,disabled:true},Validators.required),
+      'unit_price': new FormControl({value:product.unit_price,disabled:true},Validators.required),
+      'total_price': new FormControl({value:(product.quantity * product.unit_price),disabled:true},Validators.required),
+      'discount': new FormControl({value:product.discount,disabled:true},Validators.required),
+      'amount': new FormControl({value:product.amount,disabled:true},Validators.required),
     })
+  }
+
+  addNewProduct(){
+    this.products().push(this.newProduct());
+  }
+
+  removeProduct(i:number) {
+    this.products().removeAt(i);
   }
 
   //---------------- End of Quotation Products -------------------
@@ -274,18 +315,29 @@ export class EditComponent implements OnInit {
     this.termSelected = this.terms.find(x => x.id == term).no_of_days;
   }
 
-  enableEditMethod(control,index){
-    this.enableIndex = index
-    this.enableEdit = true
-    control['controls']['name'].enable()
-    control['controls']['quantity'].enable()
-    control['controls']['discount'].enable()
+  enableEditMethod(control,type){
+    console.log(type)
+    this.controller = control
+    this.enableEdit[type] = true
+    if(type == 'product'){
+      control['controls']['name'].enable()
+      control['controls']['quantity'].enable()
+      control['controls']['discount'].enable()
+    } else {
+      control.enable()
+    }
   }
 
-  disableEditMethod(control){
-    this.enableIndex = null
-    this.enableEdit = false
-    control.disable()
+  disableEditMethod(control,type){
+    this.controller = null
+    this.enableEdit[type] = false
+    if(type == 'product'){
+      control['controls']['name'].disable()
+      control['controls']['quantity'].disable()
+      control['controls']['discount'].disable()
+    } else {
+      control.disable()
+    }
   }
 
   productDetails(product,productControl){
@@ -301,7 +353,7 @@ export class EditComponent implements OnInit {
     discount = (100 - discount) / 100;
 
     product.controls.total_price.setValue(total_price);
-    product.controls.net_amount.setValue(total_price * discount);
+    product.controls.amount.setValue((total_price * discount).toFixed(2));
   }
 
   filterProduct(event) {
