4c1ee76fc2e010fab43627bcc740e7428bc98237
Seion <seion@webjunction.biz>
2021-09-24 23:05:56 +0800

    Country, Unit, Sales Target service

---
+++ src/app/_services/shared/sales-target.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { SystemConfig } from '../../config/system-config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SalesTargetService {
+
+  constructor(
+    private httpClient: HttpClient,
+  ) { }
+
+  getSalesTargetByUnit(unitUuid: string): Observable<any> {
+    return this.httpClient.get(SystemConfig.apiBaseUrl + "/sales-target/byunit/" + unitUuid).pipe();
+  }
+}
+
+// export interface Country {
+//   code: string,
+//   iso3: string,
+//   dail_code: string,
+//   currency_code: string,
+//   official_name: string,
+//   region_name: string,
+//   languages: string,
+//   is_active: boolean
+// }
