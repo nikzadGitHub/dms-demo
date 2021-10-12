a6aa232f628cbde02e529d3cc7d6120b4ebf74fa
waqas34992 <wa34992@gmail.com>
2021-10-12 14:56:10 +0500

    Resolved validation issue on searching company_name and contact_name, add popup on errors, verify and save functionality working properly

--- src/app/views/leads/leads-create/leads-create.component.ts
+++ src/app/views/leads/leads-create/leads-create.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormGroup, FormBuilder, FormArray, Validators} from '@angular/forms';
+import { FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
 import { ModalDirective } from 'ngx-bootstrap/modal';
 import { AgenciesService } from '../../settings/agencies/agencies.service';
 import { LeadsService } from '../leads.service';
@@ -13,38 +13,39 @@ import { AuthService } from '../../../auth/auth.service';
 })
 export class LeadsCreateComponent implements OnInit {
   public autoResize: boolean = true;
-  @ViewChild('successModal') successModal : ModalDirective;
-  @ViewChild('dangerModal') dangerModal : ModalDirective;
-  @ViewChild('foundModal') foundModal : ModalDirective;
-  
+  @ViewChild('successModal') successModal: ModalDirective;
+  @ViewChild('dangerModal') dangerModal: ModalDirective;
+  @ViewChild('foundModal') foundModal: ModalDirective;
+
   form: FormGroup;
-  id:any;
+  id: any;
   alertBody: string;
   alertHeader: string;
   alretType: string = 'company';
   filteredData: any = [];
   filteredCompanyData: any = [];
-  source_items:any = [
+  source_items: any = [
     'Partner',
     'Partner 1',
     'Partner 2',
   ];
-  states:any = [
+  states: any = [
     'Selangor',
     'Kuala Lumpur',
     'Perak',
     'aa'
   ];
-  items:any = [
-    {label: 'item 1'},
-    {label: 'item 2'},
-    {label: 'item 3'},
-    {label: 'item 4'},
-    {label: 'item 5'},
+  items: any = [
+    { label: 'item 1' },
+    { label: 'item 2' },
+    { label: 'item 3' },
+    { label: 'item 4' },
+    { label: 'item 5' },
   ];
   message = 'Do you want to go to opportunity add page?';
-  isSkipcompany:any = false;
-  isSkipcontact:any = false;
+  errorMessage = 'Customer already exist';
+  isSkipcompany: any = false;
+  isSkipcontact: any = false;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -54,15 +55,15 @@ export class LeadsCreateComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.form =  this.formBuilder.group({
+    this.form = this.formBuilder.group({
       //hardcode
       company_id: '',
       contact_id: '1',
-      company_name: ['aa', Validators.required],
+      company_name: ['', Validators.required],
       contact_name: '',
       department: '',
-      email: ['abc@hotmail.com', Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")],
-      mobile_number: ['aa', Validators.required],
+      email: ['', Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")],
+      mobile_number: ['', Validators.required],
       office_number: '',
       address: '',
       state: '',
@@ -81,40 +82,40 @@ export class LeadsCreateComponent implements OnInit {
       additional_notes: ''
     });
     this.form.get('prospect_type').valueChanges.subscribe(val => {
-      this.form.controls['prospect_type'].setValue(val, {emitEvent: false});
+      this.form.controls['prospect_type'].setValue(val, { emitEvent: false });
     });
     this.form.get('other_source').valueChanges.subscribe(val => {
       if (val) {
-        this.form.controls['source'].setValue(val, {emitEvent: false});
+        this.form.controls['source'].setValue(val, { emitEvent: false });
       }
     });
     this.authService.getUserSession().then(res => {
       if (res.fullname) {
-        this.form.controls['lead_owner'].setValue(res.fullname, {emitEvent: false});
+        this.form.controls['lead_owner'].setValue(res.fullname, { emitEvent: false });
       }
     });
   }
 
-  redirectPage(){
+  redirectPage() {
     this.router.navigateByUrl('leads/index');
   }
-  skip(alretType) {
+  gotoOpertunityPage(alretType) {
     if (alretType == 'company') {
       this.isSkipcompany = true;
       this.submit();
     }
   }
 
-  submit(){
+  submit() {
     let company_name = this.form.value.company_name;
     if (this.isSkipcompany) {
       this.create(true);
-      this.router.navigateByUrl('opportunity/new');
+      this.router.navigateByUrl('opportunity');
     } else {
       this.leadsService.searchCompany(company_name).subscribe(res => {
-        if(res.success) {
-          this.alertHeader = res.data;
-          this.alertBody = this.message;
+        if (res.success) {
+          this.alertHeader = this.errorMessage;
+          this.alertBody = res.message || this.message;
           this.alretType = 'company';
           this.foundModal.show();
           return;
@@ -135,30 +136,36 @@ export class LeadsCreateComponent implements OnInit {
     });
   }
 
-  verify(){
+  verify() {
     this.leadsService.store(this.form.value).subscribe(
       res => {
         this.id = res.data.id;
-      },error => {
-        this.alertBody = error.message || 'Failed';
-        this.dangerModal.show();
-      },() => {
-        this.router.navigateByUrl('leads/'+ this.id + '/verify');
-      });
+      }, error => {
+        this.alertHeader = this.errorMessage;
+        this.alertBody = error.message || this.message;
+        this.foundModal.show();
+      },
+      // () => {
+      //   this.router.navigateByUrl('leads/' + this.id + '/verify');
+      // }
+    );
   }
 
-  searchName(event){
+  searchName(event) {
     let query = event.query;
     this.leadsService.searchContact(query).subscribe(res => {
-      console.log(res);
-      if(res.success) {
-        this.filteredData = res.data;
+      if (res.success) {
+        if (res.data.length == 0) {
+          this.alertBody = 'Name is not Correct';
+          this.dangerModal.show();
+        } else {
+          this.filteredData = res.data
+        }
       }
     });
   }
 
-  onSelect(event)
-  {
+  onSelect(event) {
     console.log(event);
     if (event) {
       let contact_name = this.form.get('contact_name');
@@ -166,7 +173,7 @@ export class LeadsCreateComponent implements OnInit {
       let office_number = this.form.get('office_number');
       let postcode = this.form.get('postcode');
       let address = this.form.get('address');
-      
+
       contact_name.patchValue(event.full_name);
       mobile_number.patchValue(event.mobile_phone);
       office_number.patchValue(event.business_phone);
@@ -176,20 +183,38 @@ export class LeadsCreateComponent implements OnInit {
     }
   }
 
-  searchCompanyName(event){
-    let filtered: any[] = [];
-    let query = event.query;
-    for (let i = 0; i < this.items.length; i++) {
-      let data = this.items[i];
-      if (data.label.toLowerCase().indexOf(query.toLowerCase()) == 0) {
-        filtered.push(data);
+  searchCompanyName(event) {
+    let query = event;
+    this.leadsService.searchCompany(query).subscribe(res => {
+      if (res.success) {
+        if(res.message == 'Company already exist.'){
+          this.alertBody = 'Company already exist.';
+          this.dangerModal.show();
+        }
+        else{
+            this.alertBody = 'Company name is available to use.';
+            this.successModal.show();    
+        }
+        // this.filteredCompanyData = res.data;
       }
     }
-    this.filteredCompanyData = filtered;
+    // , error => {
+    //   this.alertBody = 'Company already exist.';
+    //   this.dangerModal.show();
+    // }
+    )
+    // let filtered: any[] = [];
+    // let query = event.query;
+    // for (let i = 0; i < this.items.length; i++) {
+    //   let data = this.items[i];
+    //   if (data.label.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+    //     filtered.push(data);
+    //   }
+    // }
+
   }
 
-  onSelectCompany(event, title)
-  {
+  onSelectCompany(event, title) {
     console.log(event);
     if (event) {
       let selectedData = event.label;
