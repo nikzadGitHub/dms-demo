1e3ede962ab943b23f8f2d6c26d6de35e9830964
Bahram Haidari <b.haidari@pomtech.com>
2021-11-01 11:09:11 +0430

    merged the demo booking with the booking entity [DMS-48]

--- src/app/dms/dms-routing.module.ts
+++ src/app/dms/dms-routing.module.ts
@@ -5,7 +5,6 @@ import { DmsComponent } from './dms.component';
 import { InventoryComponent } from './components/inventory/inventory.component';
 import { BookingsComponent } from './components/bookings/bookings.component';
 import { BookingFormComponent } from './components/bookings/booking-form/booking-form.component';
-import { BookingDemoComponent } from './components/bookings/booking-demo/booking-demo.component';
 import { ApprovalsComponent } from './components/approvals/approvals.component';
 import { BookingEntityComponent } from './components/bookings/booking-entity/booking-entity.component';
 
@@ -52,13 +51,6 @@ export const mainChildren: Routes = [
       title: 'Bookings',
     },
     children: bookingsChildren,
-  },
-  {
-    path: 'bookings/:id',
-    component: BookingDemoComponent,
-    data: {
-      title: 'Bookings Demo',
-    },
   },
   {
     path: 'approvals',
