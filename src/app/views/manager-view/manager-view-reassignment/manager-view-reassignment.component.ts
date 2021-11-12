f09b814ded8ecc9468a5ec81c1f2157a0006050d
Fahad Khancgit <fahadkhan6302@gmail.com>
2021-11-12 19:21:17 +0500

    update component for reassignment and approval

--- src/app/views/manager-view/manager-view-reassignment/manager-view-reassignment.component.ts
+++ src/app/views/manager-view/manager-view-reassignment/manager-view-reassignment.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ManagerViewReassignmentService } from './manager-view-reassignment.service';
 @Component({
   selector: 'app-manager-view-reassignment',
   templateUrl: './manager-view-reassignment.component.html',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManagerViewReassignmentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private managerviewreassignment:ManagerViewReassignmentService) { }
 
   ngOnInit(): void {
+    
+    
+     // this.contactService.getAllContactData().subscribe((data) => {
+    //   this.tableData = data["data"];
+    // });
   }
+  // getAlReassignment(){
+  //   this.managerviewreassignment.getAlReassignment().subscribe(data => {
+  //     console.log(data);
+  //   }) 
+  // }
+  
+  
 
 }
