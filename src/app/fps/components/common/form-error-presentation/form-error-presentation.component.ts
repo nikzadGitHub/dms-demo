9a5bf31b87f86d8a82ede29b75119804bfe092e2
nasernikzad <n.nikzad@pomtech.com>
2021-12-06 12:08:11 +0430

    form-error-presentation component added for UI form validation

---
+++ src/app/fps/components/common/form-error-presentation/form-error-presentation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-error-presentation',
+  templateUrl: './form-error-presentation.component.html',
+  styleUrls: ['./form-error-presentation.component.scss']
+})
+export class FormErrorPresentationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
