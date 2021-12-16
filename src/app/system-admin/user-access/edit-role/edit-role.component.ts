52d732945f0f6de40dd45d53c25a88f893f802fd
waqas34992 <wa34992@gmail.com>
2021-12-16 14:47:41 +0500

    Set text on user-loa page.

--- src/app/system-admin/user-access/edit-role/edit-role.component.ts
+++ src/app/system-admin/user-access/edit-role/edit-role.component.ts
@@ -39,8 +39,8 @@ export class EditRoleComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((params) => {
-        this.roleId = params.roleId;
-        this.getRoleDetail(this.roleId);
+      this.roleId = params.roleId;
+      this.getRoleDetail(this.roleId);
     });
     // this.route.params.subscribe((event) => {
     //   this.roleId = event.roleId;
@@ -74,6 +74,7 @@ export class EditRoleComponent implements OnInit {
           this.successModal.show();
           setTimeout(() => {
             this.successModal.hide();
+            this.router.navigateByUrl("useraccess/user-access-setup");
           }, 2000);
         },
         (error) => {
@@ -96,5 +97,23 @@ export class EditRoleComponent implements OnInit {
       }
     });
     console.log("edit:-->", this.rolePermissionsList);
+  }
+  selectAll(event) {
+    console.log("Event:", event);
+    console.log("Array(event):", Array(event));
+    if (event.checked == true) {
+      this.selectedRolePermission = Array(event).filter(
+        (value) => value.checked
+      );
+      console.log("selectedRolePermission:", this.selectedRolePermission);
+    }
+  }
+  resetPermissions() {
+    this.selectedRolePermission = [];
+    this.rolePermissionsList = [];
+  }
+  back() {
+    // this.location.back();
+    this.router.navigateByUrl("useraccess/user-access-setup");
   }
 }
