393bd7ba38419473afd4acdd427cceb2417e0b1c
nikzad2000 <m.n.nikzad2000@gmail.com>
2021-12-30 13:25:03 +0430

    Create environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  AZURE_TENANT_ID : '419c4758-e653-4ed4-b3e6-d84360e97a52',
+  AZURE_CLIENT_ID :'7a1e9113-1e9b-47cf-804a-92e07373898b'
+};
+
+export const settings = {
+  apiBaseUrl: 'http://127.0.0.1:8000/api'
+  // apiBaseUrl: "https://api.idsmed.websight-app.com/api",
+};
